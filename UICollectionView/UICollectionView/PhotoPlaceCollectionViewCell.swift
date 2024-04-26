e74df228a1638ae0d88efd4de1a20413c6f3ce96
TLiLi HAMDI <tlilios@yahoo.com>
2024-04-27 00:04:51 +0200

    in progressing

---
+++ UICollectionView/UICollectionView/PhotoPlaceCollectionViewCell.swift
@@ -0,0 +1,14 @@
+//
+//  PhotoCollectionViewCell.swift
+//  UICollectionView
+//
+//  Created by HTLILI on 26/04/2024.
+//
+
+import UIKit
+
+class PhotoPlaceCollectionViewCell: UICollectionViewCell {
+    @IBOutlet weak var photoCollectionView: UIImageView!
+    
+    
+}
---
+++ UICollectionView/UICollectionView/PhotosCollectionReusableView.swift
@@ -0,0 +1,14 @@
+//
+//  PhotosCollectionReusableView.swift
+//  UICollectionView
+//
+//  Created by HTLILI on 26/04/2024.
+//
+
+import UIKit
+
+class PhotosCollectionReusableView: UICollectionReusableView {
+    @IBOutlet weak var lblHeaderTitle: UILabel!
+    
+    
+}
