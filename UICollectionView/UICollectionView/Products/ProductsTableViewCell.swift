fdfb5cb8d1b6e8c517eb7a3d94bc1d9250c28549
TLiLi HAMDI <tlilios@yahoo.com>
2024-04-29 19:07:04 +0200

    End

--- UICollectionView/UICollectionView/Products/ProductsTableViewCell.swift
+++ UICollectionView/UICollectionView/Products/ProductsTableViewCell.swift
@@ -7,15 +7,17 @@
 
 import UIKit
 
-class ProductsTableViewCell: UITableViewCell {
-
+class ProductsTableViewCell: UITableViewCell, UICollectionViewDelegate, UICollectionViewDataSource {
+   
     @IBOutlet weak var lblTitle: UILabel!
     
     @IBOutlet weak var collectionView: UICollectionView!
     
+    var arryPhotos = [UIImage]()
     override func awakeFromNib() {
         super.awakeFromNib()
-        // Initialization code
+        collectionView.dataSource = self
+        collectionView.delegate = self
     }
 
     override func setSelected(_ selected: Bool, animated: Bool) {
@@ -23,5 +25,24 @@ class ProductsTableViewCell: UITableViewCell {
 
         // Configure the view for the selected state
     }
+    
+    func setUpCell(title: String, arrayPhotos: [UIImage]) {
+        self.lblTitle.text = title
+        self.arryPhotos = arrayPhotos
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return arryPhotos.count
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "colProductCell", for: indexPath) as! ProductsCollectionViewCell
+        cell.imgProductPhoto.image = arryPhotos[indexPath.row]
+        return cell
+    }
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+       CGSize(width: collectionView.frame.width * 0.3, height: collectionView.frame.height * 0.3)
+    }
+
 
 }
--- UICollectionView/UICollectionView/Products/ProductsViewController.swift
+++ UICollectionView/UICollectionView/Products/ProductsViewController.swift
@@ -7,7 +7,7 @@
 
 import UIKit
 
-class ProductsViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+class ProductsViewController: UIViewController, UITableViewDelegate, UITableViewDataSource, UICollectionViewDelegateFlowLayout {
    
     @IBOutlet weak var tableView: UITableView!
     var arrayProducts = [Products]()
@@ -16,16 +16,28 @@ class ProductsViewController: UIViewController, UITableViewDelegate, UITableView
         super.viewDidLoad()
         tableView.dataSource = self
         tableView.delegate = self
-        arrayProducts[Products(title: <#T##String#>, photos: <#T##[UIImage]#>)]
+        arrayProducts.append(Products(title: "Top Rated", photos: [UIImage(named: "Moto1")!, UIImage(named: "Moto2")!, UIImage(named: "Moto3")!]))
+        
+        arrayProducts.append(Products(title: "Top 4", photos: [UIImage(named: "Moto4")!, UIImage(named: "Moto5")!, UIImage(named: "Moto6")!, UIImage(named: "Moto7")!]))
+        
+        arrayProducts.append(Products(title: "Classic!", photos: [UIImage(named: "Moto8")!, UIImage(named: "Moto9")!, UIImage(named: "Moto10")!, UIImage(named: "Moto11")!, UIImage(named: "Moto12")!, UIImage(named: "Moto13")!]))
+        
+        arrayProducts.append(Products(title: "Townsman", photos: [UIImage(named: "Moto14")!, UIImage(named: "Moto15")!, UIImage(named: "Moto16")!]))
     }
 
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        <#code#>
+        arrayProducts.count
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        <#code#>
+        let cell = tableView.dequeueReusableCell(withIdentifier: "tblProductCell", for: indexPath) as! ProductsTableViewCell
+        let product = arrayProducts[indexPath.row]
+        cell.setUpCell(title: product.title, arrayPhotos: product.photos)
+        return cell
+    }
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 200
     }
 
 
